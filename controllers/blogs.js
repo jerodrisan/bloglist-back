3b5288b21667a2f3d4b9fb664fe5175b713c892c
jerodrisan <chukksiphone@gmail.com>
2023-05-27 02:02:15 +0200

    changes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,23 @@
 const blogsRouter = require('express').Router()
+const jsonFormatter = require('../utils/formatter')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async(request, response) => {     
 
     //const blogs = await Blog.find({})
     const blogs = await Blog.find({}).populate('user', { username: 1, name:1 }) //en este caso usamos en el esquema 
-    response.json(blogs)
+    //Sin salida formateada:
+    //response.json(blogs)
+    
+    //Si queremos ver la salida formateada: 
+    const JSON_blogs = JSON.stringify(blogs)
+    const obj_blogs = JSON.parse(JSON_blogs)
+    //console.log(obj_blogs)
+    let str = jsonFormatter(obj_blogs)    
+    response.send(str)    
   })
-
 
 
   /*Ejemplo de post de un blog 
@@ -20,13 +29,27 @@ blogsRouter.get('/', async(request, response) => {
      "userId": "646e575d5eb733b256be8d67"     //id del usuario que creo el blog
   }
   */
+
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+  } 
+
   blogsRouter.post('/', async (request, response) => {    
 
     let body = request.body  
     if(!body.hasOwnProperty('title') || !body.hasOwnProperty('url')){
       response.status(400).send('Bad Request') //en caso de que no esten las pop
     }else{ 
-      const user = await User.findById(body.userId) //pillamos el usuario cuya id en el post corresponde a userId
+
+      const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+      }
+      const user = await User.findById(decodedToken.id) //pillamos el usuario cuya id se encuentra identificada en el token      
       const blog = new Blog({
         title:body.title,
         author:body.author,
@@ -68,6 +91,29 @@ blogsRouter.get('/', async(request, response) => {
       console.log(excepcion)
     }
   })
+
+
+  /* Esto iria en otra carpeta
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
+  }
+
+  next(error)
+}
+}*/
 
 
   
---
+++ utils/formatter.js
@@ -0,0 +1,28 @@
+//Formateador de json para ver los datos en pantalla 
+
+const format = contactos =>{
+    let str ="[<br>"     
+    for (let i=0; i<contactos.length; i++){
+        let keys = Object.keys(contactos[i])
+        let values = Object.values(contactos[i])
+           str += ' -{<br>'
+           for(let j=0; j<keys.length; j++){
+              if(typeof values[j]==='object'){                
+                str+=`&nbsp&nbsp&nbsp&nbsp${keys[j]}:{<br>`
+                let keys2= Object.keys(values[j]) 
+                let values2=Object.values(values[j])
+                console.log(keys2, values2) 
+                for(let k=0; k<keys2.length; k++){
+                    str+= `&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp${keys2[k]}: ${values2[k]}<br>`                   
+                }
+                str+='&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp}<br>'              
+              }else{
+                str+= `&nbsp&nbsp&nbsp&nbsp${keys[j]}: ${values[j]} ,<br>`             
+              }              
+           } 
+           str+='},<br>'
+    } 
+    return str
+}
+
+module.exports =format
